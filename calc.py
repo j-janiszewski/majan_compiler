8c44c5fe20e717b62d8f075051fc874f26574b0d
Zalinafali <zalinafali@gmail.com>
2023-05-30 00:34:10 +0200

    Float, division, parenthesis, sequence of math operations for lexer and parser

--- calc.py
+++ calc.py
@@ -1,12 +1,14 @@
 import ply.lex as lex
 import ply.yacc as yacc
-from nodes import IntValue, BinOp, Variable, Instructions
+from nodes import IntValue, FloatValue, BinOp, Variable, Instructions
 
 tokens = (
+    "FLOAT_VALUE",
     "INT_VALUE",
     "PLUS",
     "MINUS",
     "TIMES",
+    "DIVIDE",
     "COMMA",
     "ID",
     "IF",
@@ -15,13 +17,18 @@ tokens = (
     "INT",
     "FLOAT",
     "BOOL",
+    "LPAREN",
+    "RPAREN",
 )
 
 
 t_PLUS = r"\+"
 t_MINUS = r"-"
 t_TIMES = r"\*"
+t_DIVIDE = r"/"
 t_COMMA = r","
+t_LPAREN = r"\("
+t_RPAREN = r"\)"
 
 reserved = {
     "if": "IF",
@@ -32,12 +39,21 @@ reserved = {
     "bool": "BOOL",
 }
 
+precedence = (
+    ('left', 'PLUS', 'MINUS'),
+    ('left', 'TIMES', 'DIVIDE'),
+)
+
 
 def t_ID(t):
     r"[a-zA-Z_][a-zA-Z_0-9]*"
     t.type = reserved.get(t.value, "ID")  # Check for reserved words
     return t
 
+def t_FLOAT_VALUE(t):
+    r"[-+]?[0-9]+\.[0-9]*"
+    t.value = float(t.value)
+    return t
 
 def t_INT_VALUE(t):
     r"([+-]?[1-9]\d*|0)"
@@ -73,10 +89,14 @@ def p_program(p):
 def p_expression_binop(p):
     """expression : expression PLUS expression
     | expression MINUS expression
-    | expression TIMES expression"""
+    | expression TIMES expression
+    | expression DIVIDE expression"""
 
     p[0] = BinOp(p[1], p[2], p[3])
 
+def p_expression_groupop(p):
+    'expression : LPAREN expression RPAREN'
+    p[0] = p[2]
 
 def p_expression_single_one(p):
     "expression_list : expression"
@@ -93,6 +113,10 @@ def p_expression_list(p):
 def p_expression_int_value(p):
     "expression : INT_VALUE"
     p[0] = IntValue(p[1])
+
+def p_expression_float_value(p):
+    "expression : FLOAT_VALUE"
+    p[0] = FloatValue(p[1])
 
 
 def p_expression_int_vars_init(p):
@@ -156,7 +180,7 @@ lexer = lex.lex()
 
 # testing
 data = """
-3 + 4 * 10 \n int num, num2, num4 \n float tuto, tiki, tson
+3 / (4. + 10) + 5 \n int num, num2, num4 \n float tuto, tiki, tson
 """
 
 
